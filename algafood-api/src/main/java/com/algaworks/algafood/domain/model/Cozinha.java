897af9773eaa9f91e454098054d461d2717c8708
claudiovdev <c.viniciusmagalhaes@gmail.com>
2023-12-24 04:12:49 +0000

    Adicionando relacionamento OneToMany 6.1

--- algafood-api/src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -1,9 +1,11 @@
 package com.algaworks.algafood.domain.model;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -19,5 +21,8 @@ public class Cozinha {
     @Column(nullable = false)
     private String nome;
 
+    @JsonIgnore
+    @OneToMany(mappedBy = "cozinha")
+    private List<Restaurante> restaurante = new ArrayList<>();
 
 }

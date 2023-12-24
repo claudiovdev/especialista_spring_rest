07e6013d51b07bd3d49a6330c9449a17523a7719
claudiovdev <c.viniciusmagalhaes@gmail.com>
2023-12-24 05:52:36 +0000

    Adicionando relacionamentos @ManyToMany 6.9

---
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/model/Produto.java
@@ -0,0 +1,36 @@
+package com.algaworks.algafood.domain.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Data
+@Entity()
+public class Produto {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private String descricao;
+
+    @Column(nullable = false)
+    private BigDecimal preco;
+
+    @Column(nullable = false)
+    private boolean ativo;
+
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    Restaurante restaurante;
+}

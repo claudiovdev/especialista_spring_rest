de01d92fbaa9cd6df1e0982ee7da8b9c8bcb320a
claudiovdev <c.viniciusmagalhaes@gmail.com>
2024-01-09 18:15:52 -0300

    Criando uma propria constraint de validação 9.15

--- algafood-api/src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -1,6 +1,7 @@
 package com.algaworks.algafood.domain.model;
 
 import com.algaworks.algafood.Groups;
+import com.algaworks.algafood.core.TaxaFrete;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -33,6 +34,7 @@ public class Restaurante {
     private String nome;
 
     //@DecimalMin("0")
+    //@TaxaFrete()
     @PositiveOrZero()
     @Column(name = "taxa_frete", nullable = false)
     private BigDecimal taxaFrete;

f63cb6ab8dc23f9e844e4df8d29f3bd446fa0a7e
claudiovdev <c.viniciusmagalhaes@gmail.com>
2024-05-14 09:01:30 -0300

    adicionando ApiModelProperty em cidade 18.11

--- algafood-api/src/main/java/com/algaworks/algafood/api/model/response/CidadeModelResponse.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/model/response/CidadeModelResponse.java
@@ -1,19 +1,20 @@
 package com.algaworks.algafood.api.model.response;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 
+@ApiModel(value = "Cidade", description = "Representa uma cidade")
 @Getter
 @Setter
 public class CidadeModelResponse {
 
+    @ApiModelProperty(value = "Id da cidade", example = "1")
     private Long id;
-
-
+    @ApiModelProperty(example = "Rio de Janeiro")
     private String nome;
-
-
     private EstadoModelResponse estado;
 }
--- algafood-api/src/main/java/com/algaworks/algafood/api/model/response/EstadoModelResponse.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/model/response/EstadoModelResponse.java
@@ -1,5 +1,6 @@
 package com.algaworks.algafood.api.model.response;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,8 +9,9 @@ import javax.validation.constraints.NotBlank;
 @Getter
 @Setter
 public class EstadoModelResponse {
+    @ApiModelProperty(example = "1")
     private Long id;
-
     @NotBlank()
+    @ApiModelProperty(example = "RJ")
     private String nome;
 }

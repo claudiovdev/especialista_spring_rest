f644934e291a7992e509277d4a0b9cda8b82cef4
claudiovdev <c.viniciusmagalhaes@gmail.com>
2024-05-14 08:40:58 -0300

    Criando tag para os endpoint e colocando descrição nos metodos

--- algafood-api/src/main/java/com/algaworks/algafood/api/controller/CidadeController.java
+++ algafood-api/src/main/java/com/algaworks/algafood/api/controller/CidadeController.java
@@ -8,6 +8,8 @@ import com.algaworks.algafood.domain.exceptions.EstadoNaoEncontradoException;
 import com.algaworks.algafood.domain.exceptions.NegocioException;
 import com.algaworks.algafood.domain.model.Cidade;
 import com.algaworks.algafood.domain.service.CidadeService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
+@Api(tags = "Cidades")
 @RestController
 @RequestMapping("/api/cidades")
 public class CidadeController {
@@ -27,11 +30,13 @@ public class CidadeController {
     @Autowired
     CidadeModelDisassembler cidadeModelDisassembler;
 
+    @ApiOperation("Lista as cidades")
     @GetMapping
     public ResponseEntity<List<CidadeModelResponse>> listar(){
         return ResponseEntity.ok().body(cidadeModelAssembler.toCollectionModelResponse(cidadeService.listar()));
     }
 
+    @ApiOperation("Busca cidade por identificador")
     @GetMapping("/{cidadeId}")
     public CidadeModelResponse buscar(@PathVariable Long cidadeId){
         return cidadeModelAssembler.toModelResponse(cidadeService.buscarCidadeExistente(cidadeId));

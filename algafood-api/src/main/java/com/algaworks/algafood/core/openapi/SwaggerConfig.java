ad216cb349002a8ca168bb4841d777b78c700e86
claudiovdev <c.viniciusmagalhaes@gmail.com>
2024-05-14 08:34:30 -0300

    escrvendo na documentação principal do swagger 18.6

--- algafood-api/src/main/java/com/algaworks/algafood/core/openapi/SwaggerConfig.java
+++ algafood-api/src/main/java/com/algaworks/algafood/core/openapi/SwaggerConfig.java
@@ -6,6 +6,7 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -21,13 +22,16 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.basePackage("com.algaworks.algafood.api"))
                 .paths(PathSelectors.any())
                 //.paths(PathSelectors.ant("/restaurantes/**"))
-                .build();
+                .build()
+                .apiInfo(apiInfo());
     }
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("API Documentation")
+                .title("Algafood API")
+                .description("Api aberta para clientes e restaurantes")
                 .version("1.0")
+                .contact(new Contact("Vinicius", "https://meusite.com.br", "contato@gmail.com"))
                 .build();
     }
 }

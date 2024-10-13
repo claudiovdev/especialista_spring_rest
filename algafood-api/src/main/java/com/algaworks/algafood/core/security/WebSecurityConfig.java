862caaebb3d013d706767cebd318735f150914c8
Claudio Magalhães <c.viniciusmagalhaes@gmail.com>
2024-10-13 14:35:25 -0300

    adicionando usuarios em memoria e add PasswordEncoder com BCryptPasswordEncoder

--- algafood-api/src/main/java/com/algaworks/algafood/core/security/WebSecurityConfig.java
+++ algafood-api/src/main/java/com/algaworks/algafood/core/security/WebSecurityConfig.java
@@ -1,10 +1,14 @@
 package com.algaworks.algafood.core.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -20,5 +24,24 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                     .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // Neste eu defino que minha aplicação será Stateless que significa que ela não vai armazenar cookies onde cada envio é um novo envio
                 .and().csrf().disable();
+    }
+
+    // Criando usuairo em memoria
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("vinicius")
+                    .password(passwordEncoder().encode("12345"))
+                    .roles("ADMIN")
+                .and()
+                    .withUser("joao")
+                    .password(passwordEncoder().encode("12345"))
+                    .roles("ADMIN");
+
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }

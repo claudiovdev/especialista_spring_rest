72fbacc12b31f6c23ce4c10351a29bc66f781c31
Claudio Magalhães <c.viniciusmagalhaes@gmail.com>
2024-10-14 11:13:26 -0300

    mensagem do commit

--- algafood-api/src/main/java/com/algaworks/algafood/core/security/WebSecurityConfig.java
+++ algafood-api/src/main/java/com/algaworks/algafood/core/security/WebSecurityConfig.java
@@ -1,14 +1,11 @@
 package com.algaworks.algafood.core.security;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+
 
 @Configuration
 @EnableWebSecurity
@@ -16,33 +13,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic()
-                .and()
+            http
+                    .csrf().disable()
                 .authorizeRequests()
-                    .antMatchers("/api/cozinhas/**").permitAll()
                     .anyRequest().authenticated()
-                .and()
-                    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // Neste eu defino que minha aplicação será Stateless que significa que ela não vai armazenar cookies onde cada envio é um novo envio
-                .and().csrf().disable();
-    }
+                    .and()
+                    .oauth2ResourceServer().opaqueToken();
 
-    // Criando usuairo em memoria
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("vinicius")
-                    .password(passwordEncoder().encode("12345"))
-                    .roles("ADMIN")
-                .and()
-                    .withUser("joao")
-                    .password(passwordEncoder().encode("12345"))
-                    .roles("ADMIN");
-
-    }
-
-    //Criando metodo de criptografia de senha
-    @Bean
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
     }
 }
--- algafood-auth/src/main/java/com/algaworks/algafood/auth/AuthorizationServerConfig.java
+++ algafood-auth/src/main/java/com/algaworks/algafood/auth/AuthorizationServerConfig.java
@@ -3,6 +3,8 @@ package com.algaworks.algafood.auth;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
@@ -19,15 +21,27 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
     @Autowired
     private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
             clients
                     .inMemory()
                         .withClient("algafood-web")
                         .secret(passwordEncoder.encode("web123"))
-                        .authorizedGrantTypes("password")
+                        .authorizedGrantTypes("password","refresh_token")
                         .scopes("write", "read")
-                    .accessTokenValiditySeconds(60 * 60 * 2);
+                    .accessTokenValiditySeconds(6 * 60 * 66)
+                    .refreshTokenValiditySeconds(60 * 24 * 60 * 60)
+                    .and()
+                        .withClient("faturamento")
+                        .secret(passwordEncoder.encode("faturamento123"))
+                        .authorizedGrantTypes("client_credentials")
+                        .scopes("write", "read")
+                    .and()
+                    .withClient("checktoken")
+                    .secret(passwordEncoder.encode("checktoken123"));
     }
 
     @Override
@@ -38,6 +52,9 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        endpoints.authenticationManager(authenticationManager);
+        endpoints.
+                authenticationManager(authenticationManager)
+                .userDetailsService(userDetailsService)
+                .reuseRefreshTokens(false);
     }
 }
